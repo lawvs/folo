e3556e8ce00a447904cf5ded7f0fbccf50f56d67
Konv Suu <2583695112@qq.com>
2025-03-25 00:14:30 +0800

    feat(desktop): add email verification prompt and translation support (#3266)

--- apps/desktop/src/renderer/src/modules/app-layout/feed-column/index.shared.tsx
+++ apps/desktop/src/renderer/src/modules/app-layout/feed-column/index.shared.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@follow/utils/utils"
 import { lazy, Suspense, useEffect, useState } from "react"
+import { useTranslation } from "react-i18next"
 import { toast } from "sonner"
 
 import { useWhoami } from "~/atoms/user"
@@ -21,7 +22,9 @@ export function NewUserGuide() {
   useEffect(() => {
     if (user?.email && !user.emailVerified) {
       toast.error(<EmailVerificationToast user={user} />, {
+        id: "email-verification",
         duration: Infinity,
+        closeButton: true,
       })
     }
   }, [user?.emailVerified])
@@ -40,10 +43,15 @@ function EmailVerificationToast({
     email: string
   }
 }) {
+  const { t } = useTranslation("settings")
   const [isEmailVerificationSent, setIsEmailVerificationSent] = useState(false)
   return (
     <div data-content className="flex w-full flex-col gap-2">
-      <div data-title>Please verify your email ({user.email}) to continue</div>
+      <div data-title>
+        {t("profile.email.verify_email", {
+          email_address: user.email,
+        })}
+      </div>
       <button
         type="button"
         data-button="true"
@@ -53,15 +61,21 @@ function EmailVerificationToast({
           isEmailVerificationSent && "!cursor-progress opacity-50",
         )}
         disabled={isEmailVerificationSent}
-        onClick={() => {
-          sendVerificationEmail({
+        onClick={async () => {
+          setIsEmailVerificationSent(true)
+          await sendVerificationEmail({
             email: user.email,
             callbackURL: `${WEB_URL}/login`,
           })
-          setIsEmailVerificationSent(true)
+          toast.dismiss("email-verification")
+          // Wait for the toast to dismiss before showing the success toast
+          // To have a better user experience
+          setTimeout(() => {
+            toast.success(t("profile.email.verification_sent"))
+          }, 800)
         }}
       >
-        Send verification email
+        {t("profile.email.send_verification")}
       </button>
     </div>
   )
