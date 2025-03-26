883d21a7dfea40563835c2307ea3ee25bb6b419a
Innei <tukon479@gmail.com>
2025-03-26 23:24:56 +0800

    feat(mobile): register InvitationScreen in sitemap for modal navigation
    Signed-off-by: Innei <tukon479@gmail.com>

--- apps/mobile/src/sitemap.tsx
+++ apps/mobile/src/sitemap.tsx
@@ -2,6 +2,7 @@ import { NavigationSitemapRegistry } from "./lib/navigation/sitemap/registry"
 import { TermsScreen } from "./screens/(headless)/terms"
 import { TwoFactorAuthScreen } from "./screens/(modal)/2fa"
 import { ForgetPasswordScreen } from "./screens/(modal)/forget-password"
+import { InvitationScreen } from "./screens/(modal)/invitation"
 import { LoginScreen } from "./screens/(modal)/login"
 import { SignUpScreen } from "./screens/(modal)/sign-up"
 import { OnboardingScreen } from "./screens/onboarding"
@@ -10,11 +11,13 @@ export function registerSitemap() {
   ;[TermsScreen].forEach((Component) => {
     NavigationSitemapRegistry.registerByComponent(Component)
   })
-  ;[LoginScreen, SignUpScreen, ForgetPasswordScreen, TwoFactorAuthScreen].forEach((Component) => {
-    NavigationSitemapRegistry.registerByComponent(Component, void 0, {
-      stackPresentation: "modal",
-    })
-  })
+  ;[LoginScreen, InvitationScreen, SignUpScreen, ForgetPasswordScreen, TwoFactorAuthScreen].forEach(
+    (Component) => {
+      NavigationSitemapRegistry.registerByComponent(Component, void 0, {
+        stackPresentation: "modal",
+      })
+    },
+  )
   ;[OnboardingScreen].forEach((Component) => {
     NavigationSitemapRegistry.registerByComponent(Component, void 0, {
       stackPresentation: "transparentModal",
