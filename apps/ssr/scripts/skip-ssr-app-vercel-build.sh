b554b7629ccf6abe49c681b0f10d871d98694918
Innei <tukon479@gmail.com>
2025-04-03 00:22:17 +0800

    fix: vercel ssr deploy script
    Signed-off-by: Innei <tukon479@gmail.com>

--- apps/ssr/scripts/skip-ssr-app-vercel-build.sh
+++ apps/ssr/scripts/skip-ssr-app-vercel-build.sh
@@ -4,27 +4,31 @@ LAST_DEPLOY_COMMIT=$(git rev-parse HEAD^)
 
 CHANGED_FILES=$(git diff --name-only $LAST_DEPLOY_COMMIT HEAD)
 
-# Define an array of paths and files to check
-CHECK_PATHS_AND_FILES=("apps/ssr/" "packages/" "package.json" "pnpm-lock.yaml")
+echo "Changed files: $CHANGED_FILES"
 
-ONLY_SERVER_OR_PACKAGES_CHANGES=true
+# Define an array of paths and files to check
+CHECK_PATHS_AND_FILES=("apps/ssr/" "packages/" "apps/ssr/package.json" "pnpm-lock.yaml" "locales/common" "locales/errors" "locales/external")
+
+ONLY_SERVER_OR_PACKAGES_CHANGES=false
 for file in $CHANGED_FILES; do
   MATCH_FOUND=false
   for path in "${CHECK_PATHS_AND_FILES[@]}"; do
-    if [[ $file == $path* ]]; then
+
+    if [[ "$file" =~ ^"$path" ]]; then
+
+      echo "Match file: $file"
       MATCH_FOUND=true
       break
     fi
   done
-  if [ "$MATCH_FOUND" = false ]; then
-    ONLY_SERVER_OR_PACKAGES_CHANGES=false
+  if [ "$MATCH_FOUND" = true ]; then
+    ONLY_SERVER_OR_PACKAGES_CHANGES=true
     break
   fi
 done
 
 if [ "$ONLY_SERVER_OR_PACKAGES_CHANGES" = true ]; then
   echo "continue"
-  echo "Changed files: $CHANGED_FILES"
   exit 1
 else
   echo "skip"
