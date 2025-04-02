48b18c20b6baf0c74077206a468fa634b8c7dfbb
Innei <tukon479@gmail.com>
2025-04-02 23:04:02 +0800

    feat(mobile): remove ThemedBlurView for android
    - Added conditional rendering for iOS and Android platforms in ThemedBlurView component.
    - Implemented a fallback View for Android with dynamic background color and opacity settings.
    - Improved code readability by utilizing the useColor hook for background color management.
    
    These changes ensure a consistent and visually appealing blur effect across both iOS and Android platforms.
    
    Signed-off-by: Innei <tukon479@gmail.com>

--- apps/mobile/src/components/common/ThemedBlurView.tsx
+++ apps/mobile/src/components/common/ThemedBlurView.tsx
@@ -2,16 +2,32 @@ import type { BlurViewProps } from "expo-blur"
 import { BlurView } from "expo-blur"
 import { useColorScheme } from "nativewind"
 import { forwardRef } from "react"
+import { Platform, StyleSheet, View } from "react-native"
+import { useColor } from "react-native-uikit-colors"
 
 export const ThemedBlurView = forwardRef<BlurView, BlurViewProps>(({ tint, ...rest }, ref) => {
   const { colorScheme } = useColorScheme()
 
-  return (
+  const background = useColor("systemBackground")
+
+  return Platform.OS === "ios" ? (
     <BlurView
       ref={ref}
       intensity={100}
       tint={colorScheme === "light" ? "systemChromeMaterialLight" : "systemChromeMaterialDark"}
       {...rest}
+    />
+  ) : (
+    <View
+      ref={ref as any}
+      {...rest}
+      style={StyleSheet.flatten([
+        rest.style,
+        {
+          backgroundColor: background,
+          opacity: 1,
+        },
+      ])}
     />
   )
 })
