75778fa0b663fd75f4319889164b832946e67c19
DIYgod <i@diygod.me>
2025-04-11 23:19:54 +0800

    fix(mobile): switching unread-only not refresh the list

--- apps/mobile/src/store/entry/hooks.ts
+++ apps/mobile/src/store/entry/hooks.ts
@@ -20,8 +20,6 @@ export const usePrefetchEntries = (props: Omit<FetchEntriesProps, "pageParam" | 
     refetchOnWindowFocus: false,
     refetchOnReconnect: false,
     enabled: !!props,
-    // 30 min
-    staleTime: 1000 * 60 * 30,
   })
 }
 export const usePrefetchEntryDetail = (entryId: string) => {
