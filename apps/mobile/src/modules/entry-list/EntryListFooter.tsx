b48a31431ecd314402cda99815d9669d1ff8f55a
Innei <tukon479@gmail.com>
2025-04-14 23:57:01 +0800

    feat(entry-list): enhance EntryListFooter with dynamic label color
    - Integrated useColor hook to dynamically set the label color for the CheckCircleCuteReIcon in EntryListFooter.
    - Adjusted Text component to include margin for better layout.
    
    Signed-off-by: Innei <tukon479@gmail.com>

--- apps/mobile/src/modules/entry-list/EntryListFooter.tsx
+++ apps/mobile/src/modules/entry-list/EntryListFooter.tsx
@@ -4,6 +4,7 @@ import { Text, TouchableOpacity } from "react-native"
 import { CheckCircleCuteReIcon } from "@/src/icons/check_circle_cute_re"
 import { getFetchEntryPayload } from "@/src/store/entry/getter"
 import { unreadSyncService } from "@/src/store/unread/store"
+import { useColor } from "@/src/theme/colors"
 
 import { useSelectedFeed, useSelectedView } from "../screen/atoms"
 import { ItemSeparator } from "./ItemSeparator"
@@ -13,11 +14,12 @@ export const EntryListFooter = () => {
   const selectedView = useSelectedView()
   const selectedFeed = useSelectedFeed()
 
+  const labelColor = useColor("label")
   return (
     <>
       <ItemSeparator />
       <TouchableOpacity
-        className="flex-row items-center gap-1.5 py-6 pl-6"
+        className="flex-row items-center justify-center gap-1.5 py-6 pl-6"
         onPress={() => {
           if (typeof selectedView === "number") {
             const payload = getFetchEntryPayload(selectedFeed, selectedView)
@@ -25,8 +27,8 @@ export const EntryListFooter = () => {
           }
         }}
       >
-        <CheckCircleCuteReIcon height={16} width={16} />
-        <Text className="text-label font-bold">
+        <CheckCircleCuteReIcon height={16} width={16} color={labelColor} />
+        <Text className="text-label ml-2 font-bold">
           {t("operation.mark_all_as_read_which", {
             which: t("operation.mark_all_as_read_which_above"),
           })}
