6301578ce656b643a814443826e48b4b63e18420
Innei <tukon479@gmail.com>
2025-04-18 13:09:31 +0800

    chore: publish config (#3467)
    Signed-off-by: Innei <tukon479@gmail.com>

---
+++ apps/mobile/bump.config.ts
@@ -0,0 +1,23 @@
+/* eslint-disable no-template-curly-in-string */
+import { defineConfig } from "nbump"
+
+export default defineConfig({
+  leading: [
+    "git pull --rebase",
+    "tsx scripts/apply-changelog.ts ${NEW_VERSION}",
+    "git add changelog",
+    "pnpm eslint --fix package.json",
+    "pnpm prettier --ignore-unknown --write package.json",
+    "git add package.json",
+  ],
+  trailing: [
+    "git checkout -b release/mobile/${NEW_VERSION}",
+    "git push origin release/mobile/${NEW_VERSION}",
+    "gh pr create --title 'release(mobile): Release v${NEW_VERSION}' --body 'v${NEW_VERSION}' --base main --head release/mobile/${NEW_VERSION}",
+  ],
+  push: false,
+  commitMessage: "release(mobile): release v${NEW_VERSION}",
+  tagPrefix: "mobile@",
+  changelog: false,
+  allowedBranches: ["dev"],
+})
