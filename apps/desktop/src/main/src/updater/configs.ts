011b7e0326246d907433e69403c6ad5481bbba99
Vixb <xzx8023@outlook.com>
2025-04-21 19:03:09 +0800

    feat: disable core updater for process from custom location (#3518)
    * feat: disable core updater for process from custom location
    
    * chore: fix lint error
    
    ---------
    
    Co-authored-by: jasonxue <jasonxue@tinnove.com.cn>

--- apps/desktop/src/main/src/updater/configs.ts
+++ apps/desktop/src/main/src/updater/configs.ts
@@ -1,9 +1,20 @@
+import path from "node:path"
+
 import { DEV } from "@follow/shared/constants"
+
+import { isWindows } from "../env"
 
 export const appUpdaterConfig = {
   // Disable renderer hot update will trigger app update when available
   enableRenderHotUpdate: !DEV,
-  enableCoreUpdate: !process.mas && !process.windowsStore,
+  enableCoreUpdate:
+    !process.mas &&
+    !process.windowsStore &&
+    // Disable core update if platfrom is windows and application is't executing from default installion path.
+    // If the process is not executed from the default installation path,
+    // it is usually managed through a package manager like Scoop.
+    // In this case, updates need to be disabled.
+    !(isWindows && path.resolve(process.execPath, "../../") !== process.env.LOCALAPPDATA),
   // Disable app update will also disable renderer hot update and core update
   enableAppUpdate: !DEV,
 
