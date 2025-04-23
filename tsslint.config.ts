59653c082d855d743d3ff0b65287162a4d4a5a7b
Johnson Chu <johnsoncodehk@gmail.com>
2025-04-24 07:22:03 +0800

    refactor(tsslint): update rules import and configuration (#3561)

--- tsslint.config.ts
+++ tsslint.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from "@tsslint/config"
-import { convertRule } from "@tsslint/eslint"
+import { convertRules } from "@tsslint/eslint"
 
 export default defineConfig({
-  rules: {
-    "no-leaked-conditional-rendering": convertRule(
-      await import("./node_modules/eslint-plugin-react-x/dist/index.mjs").then(
-        (module) => module.default.rules["no-leaked-conditional-rendering"],
-      ),
-    ),
-  },
+  rules: await convertRules({
+    "react-x/no-leaked-conditional-rendering": "error",
+  }),
 })
