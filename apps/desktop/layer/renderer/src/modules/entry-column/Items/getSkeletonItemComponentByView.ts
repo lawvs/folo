07b46a6d101493fcb0b444dfbb3a43646b47d88a
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-05-06 16:39:56 +0800

    refactor(desktop): split to avoid circular imports

--- apps/desktop/layer/renderer/src/modules/entry-column/Items/getSkeletonItemComponentByView.ts
+++ apps/desktop/layer/renderer/src/modules/entry-column/Items/getSkeletonItemComponentByView.ts
@@ -3,7 +3,7 @@ import { FeedViewType } from "@follow/constants"
 import { ArticleItemSkeleton } from "./article-item"
 import { AudioItemSkeleton } from "./audio-item"
 import { NotificationItemSkeleton } from "./notification-item"
-import { PictureItemSkeleton } from "./picture-item"
+import { PictureItemSkeleton } from "./picture-item-skeleton"
 import { SocialMediaItemSkeleton } from "./social-media-item"
 import { VideoItemSkeleton } from "./video-item"
 
