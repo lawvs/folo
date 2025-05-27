58040719396088f33b826792ce051ca1186671f1
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-05-27 14:55:45 +0800

    chore(mobile): remove babel plugin module resolver

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -5,24 +5,6 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind", unstable_transformImportMeta: true }],
       "nativewind/babel",
     ],
-    plugins: [
-      ["inline-import", { extensions: [".sql"] }],
-      [
-        "module-resolver",
-        {
-          root: ["./"],
-          alias: {
-            "es-toolkit/compat": "../../node_modules/es-toolkit/dist/compat/index.js",
-            "es-toolkit": "../../node_modules/es-toolkit/dist/index.js",
-            "better-auth/react": "../../node_modules/better-auth/dist/client/react/index.cjs",
-            "better-auth/client/plugins":
-              "../../node_modules/better-auth/dist/client/plugins/index.cjs",
-            "@better-auth/expo/client": "../../node_modules/@better-auth/expo/dist/client.cjs",
-          },
-          extensions: [".js", ".jsx", ".ts", ".tsx"],
-        },
-      ],
-      "react-native-reanimated/plugin",
-    ],
+    plugins: [["inline-import", { extensions: [".sql"] }], "react-native-reanimated/plugin"],
   }
 }
